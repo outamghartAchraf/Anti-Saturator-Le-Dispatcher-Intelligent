e416d9ff4ffb8d5e7c2c491b999b959c42d34725
amine sakhri <sakhriaminee2003@gmail.com>
2026-02-17 12:31:00 +0100

    function ajouter

--- GestionDeTache.js
+++ GestionDeTache.js
@@ -42,4 +42,52 @@ ajouter.addEventListener("click", () => {
 
 })
 
+function AfficherInfoDeTache(arr) {
+  const Container = document.getElementById("Container");
+  Container.innerHTML = "";
+
+  arr.forEach(n => {
+
+    Container.innerHTML += `
+      <div class="bg-white shadow-xl rounded-2xl p-4 mb-4">
+        <div class="flex justify-between items-start">
+
+          <div>
+            <h3 class="text-xl font-bold text-blue-600 mb-2">
+              ${n.tache}
+            </h3>
+
+            <p class="text-gray-600 mb-4">
+              ${n.description}
+            </p>
+          </div>
+
+          <button 
+            data-id="${n.id}"
+            class="delete-btn w-[30px] h-[30px]">
+            <img src="https://cdn-icons-png.flaticon.com/128/11042/11042772.png">
+          </button>
+
+        </div>
+
+        <div class="grid grid-cols-3 gap-4 text-center mt-4">
+          <div class="bg-red-100 text-red-600 py-2 rounded-xl">
+            <p class="text-sm font-semibold">Urgence</p>
+            <p class="text-lg font-bold">${n.urgence}</p>
+          </div>
+
+          <div class="bg-yellow-100 text-yellow-600 py-2 rounded-xl">
+            <p class="text-sm font-semibold">Importance</p>
+            <p class="text-lg font-bold">${n.importance}</p>
+          </div>
+
+          <div class="bg-green-100 text-green-600 py-2 rounded-xl">
+            <p class="text-sm font-semibold">Effort</p>
+            <p class="text-lg font-bold">${n.effort}</p>
+          </div>
+        </div>
+      </div>
+    `;
+  });
+}
 
