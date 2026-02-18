42e5dd1f1b433ed2d88bd415fd914c2bf04d368e
hamza aballagh <hamzaaballagh941@gmail.com>
2026-02-18 16:00:06 +0000

    add logique js de quiz

--- src/modules/quiz.js
+++ src/modules/quiz.js
@@ -1,1 +1,16 @@
-const quiz = quiz
+
+function calculer() {
+  let sliders = document.querySelectorAll("input[type=range]");
+  
+  let somme = 0;
+
+  sliders.forEach(slider => {
+    somme += Number(slider.value);
+  });
+
+  let moyenne = somme / sliders.length;
+
+  document.getElementById("result").innerHTML =
+    "Somme : " + somme + "<br>Moyenne : " + moyenne.toFixed(2);
+}
+
