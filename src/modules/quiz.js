af74203f38468f8b00506a5cc3d8405f8f76f5e1
hamza aballagh <hamzaaballagh941@gmail.com>
2026-02-19 12:38:34 +0000

    add the slid to rating the teh question

--- src/modules/quiz.js
+++ src/modules/quiz.js
@@ -2,15 +2,6 @@
 function calculer() {
   let sliders = document.querySelectorAll("input[type=range]");
   
-  let somme = 0;
 
-  sliders.forEach(slider => {
-    somme += Number(slider.value);
-  });
-
-  let moyenne = somme / sliders.length;
-
-  document.getElementById("result").innerHTML =
-    "Somme : " + somme + "<br>Moyenne : " + moyenne.toFixed(2);
 }
 
