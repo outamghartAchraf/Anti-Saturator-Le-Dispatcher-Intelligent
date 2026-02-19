722bba25fa065996c7a1f1c714430ccb99c9f53e
hamza aballagh <hamzaaballagh941@gmail.com>
2026-02-19 12:45:35 +0000

    finihed the worck in the file quiz and test the code to calculatb the moy the energy

--- src/modules/quiz.js
+++ src/modules/quiz.js
@@ -1,4 +1,7 @@
 export function calculateEnergy() {
   const physical = +document.getElementById("physical").value;
   const mental = +document.getElementById("mental").value;
- 
+  const motivation = +document.getElementById("motivation").value;
+
+  return Math.round((physical + mental + motivation) / 3);
+}
