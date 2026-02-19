7ca4ec4c2f44b45c5a630754ffff2cf6418b2096
hamza aballagh <hamzaaballagh941@gmail.com>
2026-02-19 12:39:32 +0000

    ad functio to calculat the sum

--- src/modules/quiz.js
+++ src/modules/quiz.js
@@ -2,6 +2,11 @@
 function calculer() {
   let sliders = document.querySelectorAll("input[type=range]");
   
+  let somme = 0;
 
-}
+  sliders.forEach(slider => {
+    somme += Number(slider.value);
+  });
+
+
 
